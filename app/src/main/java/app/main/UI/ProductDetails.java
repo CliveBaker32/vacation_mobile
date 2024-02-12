658ad07d9d933bb9cec88f50c38fa02afa473190
Clive Baker <cbak585@wgu.edu>
2024-02-11 21:12:34 -0500

    Finished webinar 1.

--- app/src/main/java/app/main/UI/ProductDetails.java
+++ app/src/main/java/app/main/UI/ProductDetails.java
@@ -2,7 +2,11 @@ package app.main.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import app.main.R;
 
@@ -12,5 +16,14 @@ public class ProductDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProductDetails.this, PartDetails.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/app/main/UI/ProductList.java
+++ app/src/main/java/app/main/UI/ProductList.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -27,5 +30,29 @@ public class ProductList extends AppCompatActivity {
         });
 
         System.out.println(getIntent().getStringExtra("test"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_product_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.mysample) {
+            Toast.makeText(ProductList.this, "put in sample data", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+
+//            Intent intent = new Intent(ProductList.this, ProductDetails.class);
+//            startActivity(intent);
+
+            return true;
+        }
+        return true;
     }
 }
