1973c22795293e40f04c752f75515beda61b4792
Clive Baker <cbak585@wgu.edu>
2024-02-17 21:13:37 -0500

    Able to click on Part list recycle view and move to next screen.

--- app/src/main/java/app/main/UI/ProductDetails.java
+++ app/src/main/java/app/main/UI/ProductDetails.java
@@ -1,6 +1,8 @@
 package app.main.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,9 +10,15 @@ import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import app.main.R;
+import app.main.database.Repository;
+import app.main.entities.Part;
 
 public class ProductDetails extends AppCompatActivity {
+    Repository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +33,15 @@ public class ProductDetails extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        RecyclerView recyclerView = findViewById(R.id.partrecyclerview);
+        repository = new Repository(getApplication());
+        final PartAdapter partAdapter = new PartAdapter(this);
+        recyclerView.setAdapter(partAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        partAdapter.setParts(repository.getAllParts());
+
+
     }
 }
